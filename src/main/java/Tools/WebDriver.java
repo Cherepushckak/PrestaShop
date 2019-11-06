5dcdf4c7a2d980557678c0de45c63c5f9b4a461c
Oleg Kmet` <39024709+Goriander@users.noreply.github.com>
2019-11-06 18:40:16 +0200

    Added web driver (#21)

---
+++ src/main/java/Tools/WebDriver.java
@@ -0,0 +1,11 @@
+package Tools;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class WebDriver {
+    static public ChromeDriver driver = new ChromeDriver();
+    static public final String startURL = "http://3.124.147.74/";  //Default URL for site
+
+    public WebDriver(){
+    }
+}
