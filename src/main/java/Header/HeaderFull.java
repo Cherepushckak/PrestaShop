f01024a393d3156de3e837f120c02701fa4e23cf
VolodymyrRomanovych <volodymyr.romanovych@gmail.com>
2019-11-07 01:44:32 +0200

    Minimally edited 'User' packege and some improvements to HeaderFull.java

--- src/main/java/Header/HeaderFull.java
+++ src/main/java/Header/HeaderFull.java
@@ -1,6 +1,9 @@
 package Header;
 
 import java.util.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import Tools.WebDriver;
 
 /**
  * Class HeaderFull is a child class of Header with it;s own fields: clothes, accessories,
@@ -16,8 +19,12 @@ public class HeaderFull extends Header {
     private WebElement language;
     private WebElement cart;
     private WebElement currency;
+    private WebElement searchProductField;
+    private WebElement searchProductButton;
+    private WebElement desktop_user_info;
     private LogUserInfo logUserInfo;
     private UnlogUserInfo unlogUserInfo;
+
     /**
      * Default constructor
      */
@@ -50,18 +57,19 @@ public class HeaderFull extends Header {
          return false
          */
         if (logUserInfo) {
-            this.logUserInfo
+            this.logUserInfo;
             return result;
         }
     }
 
     /** Method for constructor, creates part of the HeaderFull Object*/
     public void createHeaderFull() {
-        searchProductField = driver.findElement(By.cssSelector(".ui-autocomplete-input"));
-        searchProductButton = driver.findElement(By.xpath("//button[@type='submit']"));
-        language = driver.findElement(By.cssSelector("//div[@id='_desktop_language_selector']//span[@class='expand-more']"));
-        currency = driver.findElement(By.cssSelector("#currency-selector-label"));
-        cart = driver.findElement(By.cssSelector(".header"));
+        searchProductField = WebDriver.driver.findElement(By.cssSelector(".ui-autocomplete-input"));
+        searchProductButton = WebDriver.driver.findElement(By.xpath("//button[@type='submit']"));
+        language = WebDriver.driver.findElement(By.cssSelector("//div[@id='_desktop_language_selector']//span[@class='expand-more']"));
+        currency = WebDriver.driver.findElement(By.cssSelector("#currency-selector-label"));
+        desktop_user_info = WebDriver.driver.findElement(By.cssSelector(".aHS-bnu>div:nth-child(2)>span>a", "#_desktop_user_info"));
+        cart = WebDriver.driver.findElement(By.cssSelector(".header"));
 
     }
     private boolean checkCart(){
