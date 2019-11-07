5735fccf767ff5ca31f63e31f98fb1ffc04379b1
Admin <decikfettire@gmail.com>
2019-11-07 18:33:50 +0200

    Improoving LogiForm

--- src/main/java/Tools/WebDriver.java
+++ src/main/java/Tools/WebDriver.java
@@ -1,11 +1,15 @@
-package Tools;
+package tools;
 
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class WebDriver {
-    static public ChromeDriver driver = new ChromeDriver();
+    static public ChromeDriver driver;
     static public final String startURL = "http://3.124.147.74/";  //Default URL for site
-
+    
     public WebDriver(){
+        System.setProperty ( "webdriver.chrome.driver" ,
+                "C:\\Users\\Admin\\Desktop\\PrestaShop\\src\\main\\java\\Data\\chromedriver.exe" );
+        driver = new ChromeDriver (  );
     }
+    
 }
