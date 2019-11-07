d6140825393efd2fcfb2cb4a4140f54633505faa
Oleg Kmet` <39024709+Goriander@users.noreply.github.com>
2019-11-07 20:03:43 +0200

    Search page (#40)
    * Initialize variables
    
    * Write page path code
    
    * Added sort by
    
    * add some code in categories
    
    * fix .gitignore
    
    * added changes in .gitignore...

--- src/main/java/pages/Search/SortBy.java
+++ src/main/java/pages/Search/SortBy.java
@@ -1,14 +1,35 @@
 package pages.Search;
 
+import org.openqa.selenium.WebElement;
+import Tools.WebDriver;
+import java.util.ArrayList;
+
 /**
- * 
+ * Use method selectSort, with int argument variant of sorting
+ * Use template:
+ * Sort by:
+ *      Relevance           > 0
+ *      Name, A to Z        > 1
+ *      Name, Z to A        > 2
+ *      Price, low to high  > 3
+ *      Price, high to low  > 4
  */
 public class SortBy {
 
+    private ArrayList<WebElement> sortType;
     /**
      * Default constructor
      */
     public SortBy() {
+        sortType = (ArrayList<WebElement>) WebDriver.driver.findElementByXPath("//div[@class='dropdown-menu']//a");
+    }
+
+    /**
+     * Maybe I will need to rewrite this method in future, if test will be fallen
+     * @param index
+     */
+    public void selectSort(int index){
+        sortType.get(index).click();
     }
 
 
