6cd14e88dd617176eb51d89d7f95ca030481f369
Admin <decikfettire@gmail.com>
2019-11-07 20:37:14 +0200

    Adding webdriver

--- src/main/java/tools/WebDriver.java
+++ src/main/java/tools/WebDriver.java
@@ -3,9 +3,12 @@ package tools;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class WebDriver {
-    static public ChromeDriver driver = new ChromeDriver();
+    static public ChromeDriver driver;
     static public final String startURL = "http://3.124.147.74/";  //Default URL for site
-
-    public WebDriver(){
+    
+    public WebDriver (){
+        System.setProperty ( "webdriver.chrome.driver" ,
+                "..\\PrestaShop\\src\\main\\java\\data\\chromedriver.exe" );
+        driver = new ChromeDriver (  );
     }
 }
