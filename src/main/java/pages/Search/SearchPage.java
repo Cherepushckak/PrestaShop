520d71e2d2ba8d776eddda4b639c14364f5de616
Goriander <Oleg23.OK@gmail.com>
2019-11-07 17:58:14 +0200

    Initialize variables

--- src/main/java/pages/Search/SearchPage.java
+++ src/main/java/pages/Search/SearchPage.java
@@ -1,20 +1,28 @@
 package pages.Search;
 
+import org.openqa.selenium.WebElement;
+import pages.Common.APageParent;
+
 /**
  * 
  */
 public class SearchPage extends APageParent {
-
+    //elements
+    private WebElement Description;
+    //instants
+    private Categories categories;
+    private Description description;
+    private SortBy sortBy;
+    private ActiveFilter activeFilter;
+    //containers
+    private Filter filter;
+    private Products products;
     /**
      * Default constructor
      */
     public SearchPage() {
+        //createHeader();
+
     }
-
-
-
-
-
-
 
 }
