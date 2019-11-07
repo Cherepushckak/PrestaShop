d070484dad0599c0036f84e73bed889aa48bd0ed
Oleg-Decik <48534395+Oleg-Decik@users.noreply.github.com>
2019-11-07 23:34:31 +0200

    Login form (#45)
    * Adding chromedriver
    
    * Improove 'LoginForm' class

--- src/main/java/pages/Login/LoginForm.java
+++ src/main/java/pages/Login/LoginForm.java
@@ -1,10 +1,12 @@
 package pages.Login;
 
-import pages.Register.RegisterForm;
+import tools.WebDriver;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
 /**
- * 
+ *
  */
 public class LoginForm {
     private WebElement email;
@@ -12,18 +14,54 @@ public class LoginForm {
     private WebElement show;
     private WebElement forgotPassword;
     private WebElement signIn;
-    private RegisterForm registerForm;
+    private WebElement noAccount;
     
     
     /**
      * Default constructor
      */
-    public LoginForm() {
-        initLogin ( e, p );
+    public LoginForm () {
+        initLogin ();
     }
     
-    public void initLogin(WebElement email, WebElement password){
-        this.email = email;
-        this.password = password;
+    public LoginForm ( String email, String password ) {
+        
+        emailClick ().sendKeys ( email );
+        passwordClick ().sendKeys ( password );
+        
+        
+    }
+    
+    public void initLogin () {
+        WebDriver webDriver = new WebDriver ();
+        webDriver.driver.get ( "http://3.124.147.74/index.php?controller=authentication&back=my-account" );
+        writeDefaultCredantional ();
+        showClick ();
+    }
+    
+    public WebElement emailClick () {
+        WebElement clickEmail = WebDriver.driver.findElement ( By.cssSelector ( "#login-form > section > " +
+                "div:nth-child(2) input" ) );
+        clickEmail.click ();
+        return clickEmail;
+    }
+    
+    public WebElement passwordClick () {
+        WebElement clickPassword = WebDriver.driver.findElement ( By.cssSelector ( "#login-form > section > " +
+                "div:nth-child(3) > div.col-md-6 > div > input" ) );
+        clickPassword.click ();
+        return clickPassword;
+    }
+    
+    public WebElement showClick (  ){
+        WebElement clickShow = WebDriver.driver.findElement ( By.cssSelector ( "#login-form > section > " +
+                "div:nth-child(3) > div.col-md-6 > div > span > button" ) );
+        clickShow.click ();
+        return clickShow;
+    }
+    
+    public void writeDefaultCredantional () {
+        emailClick ().sendKeys ( "admin@gmail.com" );
+        passwordClick ().sendKeys ( "admin" );
     }
 }
