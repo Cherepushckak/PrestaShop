0da4e0c1238c06b02236256b61e4f1b856f1e207
Goriander <Oleg23.OK@gmail.com>
2019-11-08 18:07:53 +0200

    Write some code

--- src/main/java/pages/Search/Categories.java
+++ src/main/java/pages/Search/Categories.java
@@ -3,11 +3,7 @@ package main.java.pages.Search;
 import main.java.tools.WebDriver;
 import org.openqa.selenium.WebElement;
 import java.util.ArrayList;
-<<<<<<< HEAD
-=======
 import java.util.Locale;
-
->>>>>>> c5217616128c234952f78025e319a8de3d33d7b2
 /**
  * 
  */
@@ -24,7 +20,7 @@ public class Categories {
         ArrayList<Category> temp;
 
         for (WebElement c: WebDriver.driver.findElementsByXPath("//li[@data-depth='0']")) {
-            categories.add(new Category(c));
+            //categories.add(new Category(c));
         }
         //categories = (ArrayList<WebElement>) WebDriver.driver.findElementsByXPath("//li[@data-depth=0]/a");
     }
