fb3b4c7559c082014478191d983fa421a0527259
VolodymyrRomanovych <volodymyr.romanovych@gmail.com>
2019-11-09 08:59:13 +0200

    Address

--- src/main/java/pages/addresses/Address.java
+++ src/main/java/pages/addresses/Address.java
@@ -1,15 +1,16 @@
 package main.java.pages.addresses;
 
+import main.java.tools.WebDriver;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
 /**
- * 
+ * Address class
  */
 public class Address {
 
 	// Fields
-	private WebElement addressesContainer;
+	private WebElement addressContainer;
 	private WebElement alias;
 	private WebElement allFieldsAddress;
 	private WebElement updateButton;
@@ -18,43 +19,29 @@ public class Address {
 	/**
 	 * Default constructor
 	 */
-	public Address(WebElement addressesContainer) {
-		this.addressesContainer = addressesContainer;
-		initAddresses();
+	public Address(WebElement addressContainer) {
+		this.addressContainer = WebDriver.driver.findElement(By.xpath("//article[@id='address-13']/div[1]/address"));
+		initAddress();
 	}
-	
-	public void initAddresses () {
-		alias = addressesContainer.findElement(By.xpath("//article[@id='address-13']/div[1]/h4"));
-		allFieldsAddress = addressesContainer.findElement(By.xpath("//article[@id='address-13']/div[1]/address"));
-		updateButton = addressesContainer.findElement(By.cssSelector(".address-footer>a:nth-child(1)"));
-		deleteButton = addressesContainer.findElement(By.cssSelector(".address-footer>a:nth-child(2)"));
+
+	// Init address
+	public void initAddress() {
+		alias = WebDriver.driver.findElement(By.xpath("//article[@id='address-13']/div[1]/h4"));
+		allFieldsAddress = WebDriver.driver.findElement(By.xpath("//article[@id='address-13']/div[1]/address"));
+		updateButton = WebDriver.driver.findElement(By.cssSelector(".address-footer>a:nth-child(1)"));
+		deleteButton = WebDriver.driver.findElement(By.cssSelector(".address-footer>a:nth-child(2)"));
 	}
 
 	// Getters and setters
-	public WebElement getAddressesContainer() {
-		return addressesContainer;
-	}
+	public WebElement getAddressContainer() { return addressContainer; }
+	public void setAddressContainer(WebElement addressContainer) { this.addressContainer = addressContainer; }
 
-	public void setAddressesContainer(WebElement addressesContainer) {
-		this.addressesContainer = addressesContainer;
-	}
+	public WebElement getAlias() { return alias; }
+	public void setAlias(WebElement alias) { this.alias = alias; }
 
-	public WebElement getAlias() {
-		return alias;
-	}
+	public WebElement getAllFieldsAddress() { return allFieldsAddress; }
+	public void setAllFieldsAddress(WebElement allFieldsAddress) { this.allFieldsAddress = allFieldsAddress; }
 
-	public void setAlias(WebElement alias) {
-		this.alias = alias;
-	}
-
-	public WebElement getAllFieldsAddress() {
-		return allFieldsAddress;
-	}
-
-	public void setAllFieldsAddress(WebElement allFieldsAddress) {
-		this.allFieldsAddress = allFieldsAddress;
-	}
-	
 	// Click 'Update' button
     public void updateClick() {
     	updateButton.click();
--- src/main/java/pages/addresses/Addresses.java
+++ src/main/java/pages/addresses/Addresses.java
@@ -1,11 +1,19 @@
 package main.java.pages.addresses;
 
+import main.java.tools.WebDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
 /**
  * 
  */
 public class Addresses {
 
-	
+    // Fields
+    private WebElement path;
+
+    private WebElement createNewAddressButton;
+
     /**
      * Default constructor
      */
@@ -14,7 +22,14 @@ public class Addresses {
 //
 //    	addresses.add();
 //    }
-    
-    
+
+    public void createNewAddressButton() {
+        createNewAddressButton = WebDriver.driver.findElement(By.xpath("//section[@id='content']/div[4]/a"));
+    }
+
+    // Click '+ Create new address' button
+    public void createNewAddressClick() {
+        createNewAddressButton.click();
+    }
     
 }
