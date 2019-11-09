61c7a99a6c7056ae04c3c297c2e8153ee84b9280
ArtemHurin <49883253+ArtemHurin@users.noreply.github.com>
2019-11-09 23:11:42 +0200

    Product and Cart changes (#66)
    * draft file, some changes in Product
    
    * updated classes belongs to Cart
    
    * fixed conflicts
    
    * few not important changes on product and cart classes

---
+++ src/main/java/pages/cart/ShoppingItem.java
@@ -0,0 +1,116 @@
+package main.java.pages.cart;
+
+import main.java.pages.product.ProductPage;
+import main.java.tools.WebDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+/**
+ * 
+ */
+public class ShoppingItem {
+    /**
+     * container of one item
+     */
+
+    private WebElement shoppingItemContainer;
+    /**
+     * fields
+     */
+    private WebElement name;
+    private WebElement increaseQuantity;
+    private WebElement decreaseQuantity;
+    private WebElement price;
+    private WebElement totalPrice;
+    private WebElement basket;
+    private WebElement quantity;
+
+
+
+    /**
+     * Default constructor
+     */
+    public ShoppingItem(WebElement shoppingItemContainer) {
+        initShoppingItem();
+    }
+
+    /**
+     * initialization
+     */
+
+    private void initShoppingItem () {
+        name = WebDriver.driver.findElement(By.cssSelector(".product-line-info>.label"));
+        increaseQuantity = WebDriver.driver.findElement(By.cssSelector(".material-icons.touchspin-up"));
+        decreaseQuantity = WebDriver.driver.findElement(By.cssSelector(".material-icons.touchspin-down"));
+        price = WebDriver.driver.findElement(By.cssSelector(".current-price>.price"));
+        totalPrice = WebDriver.driver.findElement(By.cssSelector(".col-md-6.col-xs-2.price>.product-price"));
+        basket = WebDriver.driver.findElement(By.cssSelector(".material-icons.float-xs-left"));
+        quantity = WebDriver.driver.findElement(By.cssSelector(".js-cart-line-product-quantity.form-control"));
+    }
+
+
+    /**
+     * getters
+     */
+
+    public WebElement getShoppingItemContainer() {
+        return shoppingItemContainer;
+    }
+
+    public WebElement getName() {
+        return name;
+    }
+
+    public WebElement getIncreaseQuantity() {
+        return increaseQuantity;
+    }
+
+    public WebElement getDecreaseQuantity() {
+        return decreaseQuantity;
+    }
+
+    public WebElement getPrice() {
+        return price;
+    }
+
+    public WebElement getTotalPrice() {
+        return totalPrice;
+    }
+
+    public WebElement getBasket() {
+        return basket;
+    }
+
+    public String getNameText () {
+       return getName().getText();
+    }
+
+    public double getPriceValue() {
+        return Double.parseDouble(getPrice().getText().replace("₴", "").trim());
+    }
+
+    public int getQuantity () {
+        return Integer.parseInt(quantity.getCssValue("value"));
+    }
+
+    public double getTotalPriceValue () {
+        return Double.parseDouble(getTotalPrice().getText().replace("₴", "").trim());
+    }
+
+    public void clickIncreaseQuantity () {
+        increaseQuantity.click();
+    }
+
+    public void clickDecreaseQuantity () {
+        decreaseQuantity.click();
+    }
+
+    public ProductPage clickName () {
+        name.click();
+        return new ProductPage();
+    }
+
+    public void clickBasket () {
+        basket.click();
+    }
+}
--- src/main/java/tools/WebDriver.java
+++ src/main/java/tools/WebDriver.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class WebDriver {
     static public ChromeDriver driver;
-    static public final String startURL = "http://3.124.147.74/";  //Default URL for site
+    static public final String startURL = "http://3.124.147.74";  //Default URL for site
     
     public WebDriver (){
         System.setProperty ( "webdriver.chrome.driver" ,
