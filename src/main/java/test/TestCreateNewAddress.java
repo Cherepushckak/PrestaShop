a8b674446e0526e0d04e6f3d77678c375d1178d8
VolodymyrRomanovych <volodymyr.romanovych@gmail.com>
2019-11-09 23:15:21 +0200

    Added test to run in test suite

---
+++ src/main/java/test/TestCreateNewAddress.java
@@ -0,0 +1,12 @@
+package main.java.test;
+
+import static org.testng.Assert.*;
+import org.testng.annotations.Test;
+import main.java.tools.WebDriver;
+
+public class TestCreateNewAddress {
+    @Test(priority = 1)
+    public void createNewAddress() {
+        System.out.println("It WORKs! =D");
+    }
+}
