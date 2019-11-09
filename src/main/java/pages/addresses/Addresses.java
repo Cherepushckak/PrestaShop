de8e122ca49577167dec490127c50c166898d15b
VolodymyrRomanovych <volodymyr.romanovych@gmail.com>
2019-11-09 10:31:26 +0200

    Addresses.java done

--- src/main/java/pages/addresses/Addresses.java
+++ src/main/java/pages/addresses/Addresses.java
@@ -1,8 +1,8 @@
 package main.java.pages.addresses;
 
 import main.java.tools.WebDriver;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import java.util.ArrayList;
 
 /**
  * 
@@ -10,26 +10,80 @@ import org.openqa.selenium.WebElement;
 public class Addresses {
 
     // Fields
-    private WebElement path;
-
-    private WebElement createNewAddressButton;
+    private WebElement homeTopLink;
+    private WebElement yourAccountLink;
+    private String actualPageName;
+    private WebElement createNewAddressLink;
+    private WebElement backToYourAccountLink;
+    private WebElement homeLowerLink;
 
     /**
      * Default constructor
      */
-//    public Addresses() {
-//    	ArrayList<Addresses> addresses = new ArrayList<Addresses>();
-//
-//    	addresses.add();
-//    }
-
-    public void createNewAddressButton() {
-        createNewAddressButton = WebDriver.driver.findElement(By.xpath("//section[@id='content']/div[4]/a"));
+    public Addresses() {
+        homeTopLink();
+        yourAccountLink();
+        actualPageName();
+    	ArrayList<Address> addresses = new ArrayList<>();
+        initAddressesList(addresses);
+        createNewAddressLink();
+        backToYourAccountLink();
+        homeLowerLink();
     }
 
-    // Click '+ Create new address' button
+    private void homeTopLink() {
+        homeTopLink = WebDriver.driver.findElementByXPath("//section[@id='wrapper']/div/nav/ol/li[1]/a/span");
+    }
+
+    private void yourAccountLink() {
+        yourAccountLink = WebDriver.driver.findElementByXPath("//section[@id='wrapper']/div/nav/ol/li[2]/a/span");
+    }
+
+    private void actualPageName() {
+        actualPageName = WebDriver.driver.findElementByXPath("//*[@id='main']/header/h1/text()").getText();
+    }
+
+    private void initAddressesList(ArrayList<Address> addresses) {
+        for (WebElement current : WebDriver.driver.findElementsByCssSelector("div.col-sm-6:nth-child(2)")) {
+            addresses.add(new Address(current));
+        }
+    }
+
+    private void createNewAddressLink() {
+        createNewAddressLink = WebDriver.driver.findElementByXPath("//section[@id='content']/div[4]/a");
+    }
+
+    private void backToYourAccountLink() {
+        backToYourAccountLink = WebDriver.driver.findElementByXPath("//section[@id='main']/footer/a[1]");
+    }
+
+    private void homeLowerLink() {
+        homeLowerLink = WebDriver.driver.findElementByXPath("//section[@id='main']/footer/a[2]");
+    }
+
+    // Click 'Home top' link
+    public void homeTopClick() {
+        homeTopLink.click();
+    }
+
+    // Click 'Your account' link
+    public void yourAccountClick() {
+        yourAccountLink.click();
+    }
+
+    // Click '+ Create new address' link
     public void createNewAddressClick() {
-        createNewAddressButton.click();
+        createNewAddressLink.click();
+    }
+
+    // Click '< Back to your account' link
+    public void backToYourAccountClick() {
+        backToYourAccountLink.click();
+    }
+
+    // Click 'Home Lower' link
+    public void homeClick() {
+        homeLowerLink.click();
     }
     
 }
