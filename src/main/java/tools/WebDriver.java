2aae4a2f3ea3002736dcb4c86accfccd47918fcf
VolodymyrRomanovych <volodymyr.romanovych@gmail.com>
2019-11-10 19:29:48 +0200

    comment

--- src/main/java/tools/WebDriver.java
+++ src/main/java/tools/WebDriver.java
@@ -5,7 +5,6 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class WebDriver {
     static public ChromeDriver driver;
     private static final String startURL = "http://3.124.147.74"; // Default URL for site
-    
 
     public WebDriver() {
         System.setProperty("webdriver.chrome.driver",
@@ -13,7 +12,6 @@ public class WebDriver {
         driver = new ChromeDriver();
         driver.manage().window().maximize(); // Maximize test browser window
         driver.get(startURL); // GoTo startURL
-
     }
     public void closeDriver() { driver.quit(); } // Close test browser windows to release hardware resources
 }
