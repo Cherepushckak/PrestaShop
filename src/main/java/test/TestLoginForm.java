1ee6459cf821f37af984bdb59476330575f7eb43
Oleg-Decik <48534395+Oleg-Decik@users.noreply.github.com>
2019-11-11 23:19:04 +0200

    Adding screenshots to allure and common class 'test' (#86)

--- src/main/java/test/TestLoginForm.java
+++ src/main/java/test/TestLoginForm.java
@@ -5,31 +5,21 @@ import static org.testng.Assert.*;
 import io.qameta.allure.*;
 import main.java.pages.header.UnlogUserInfo;
 import main.java.pages.user.UserPage;
-import main.java.tools.OurWebDriver;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
+@Listeners (test.class)
 
 
-public class TestLoginForm {
-	@BeforeTest
-	private void setUpWebDriver(){
-		OurWebDriver webDriver = new OurWebDriver();
-	}
-	
-	@AfterTest
-	private void closeWebDriver(){
-		OurWebDriver.driver.quit ();
-	}
+public class TestLoginForm extends test {
 	
 	@Severity ( SeverityLevel.CRITICAL )
-	@Description( "Verify if customer can log in" )
+	@Description ( "Verify if customer can log in" )
 	@Test
 	public void testLoginForm () {
-		UserPage userPage = new UnlogUserInfo ().clickSignIn () .getLoginForm()
-				.logIn ("admin@gmail.com", "admin" );
-		String expected = "Admin admin";
-		String actual = userPage.createHeader ().getLogUserInformation ().getLogUserInfo ().getUserNameFromHeader();
+		UserPage userPage = new UnlogUserInfo ().clickSignIn ().getLoginForm ()
+				.logIn ( "admin@gmail.com", "admin" );
+		String expected = "Admin ";
+		String actual = userPage.createHeader ().getLogUserInformation ().getLogUserInfo ().getUserNameFromHeader ();
 		assertEquals ( actual, expected );
+		
 	}
 }
