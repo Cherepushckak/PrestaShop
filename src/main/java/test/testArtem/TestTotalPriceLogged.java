00fd46975559d673ed2b5f95eb71c16940216778
ArtemHurin <49883253+ArtemHurin@users.noreply.github.com>
2019-11-13 20:47:55 +0200

    added some tests (#111)

---
+++ src/main/java/test/testArtem/TestTotalPriceLogged.java
@@ -0,0 +1,25 @@
+package main.java.test.testArtem;
+
+import main.java.helperInstrument.AddToCartHelper;
+import main.java.helperInstrument.LogInHelper;
+import main.java.pages.cart.CartPage;
+import main.java.pages.cart.ShoppingItem;
+import main.java.pages.main.MainPage;
+import main.java.test.test;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+public class TestTotalPriceLogged extends test {
+
+    @Test
+    public void testPriceLogged () {
+        MainPage userPage = new LogInHelper().getUserPage().getLogCont().clickLogo();
+        CartPage cartPage = new AddToCartHelper().getCartPage();
+        cartPage.getShoppingCart().getShoppingItemsList().get(0).clickIncreaseQuantity();
+        ShoppingItem fistItem = cartPage.getShoppingCart().getShoppingItemsList().get(0);
+        double expected = fistItem.getPriceValue() * fistItem.getQuantity();
+        double actual = fistItem.getTotalPriceValue();
+        assertEquals(actual, expected);
+    }
+}
