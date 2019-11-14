9faaec416e5894bdfe8cc870c44b088f26befbe0
ShBr2k01 <ShBr2k@gmail.com>
2019-11-15 00:44:34 +0200

    ProductReviewFeature.01

--- src/main/java/pages/product/Reviews.java
+++ src/main/java/pages/product/Reviews.java
@@ -1,17 +1,37 @@
 package main.java.pages.product;
 
+import main.java.tools.OurWebDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
 /**
- * 
+ *
  */
 public class Reviews {
+
+    private WebElement buttonCreateReview;
 
     /**
      * Default constructor
      */
     public Reviews() {
+        initReviews();
     }
 
 
+    public void initReviews () {
+        buttonCreateReview = OurWebDriver.driver.findElement(By.xpath("//a[@class='btn btn-primary'][contains(text(),'Be the first to write a review!')]"));
+    }
 
+
+    public WebElement getButtonCreateReview () {
+        return buttonCreateReview;
+    }
+
+
+    public ManageReview clickButtonCreateReview(boolean isNewReview, String rating, String textTitleReview, String textDetailReview) {
+        buttonCreateReview.click();
+        return new ManageReview(isNewReview, rating, textTitleReview, textDetailReview);
+    }
 
 }
