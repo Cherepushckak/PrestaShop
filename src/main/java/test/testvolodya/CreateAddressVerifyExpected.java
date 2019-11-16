a8f646889dadf9466fd97e96ea77217667d43c1c
Goriander <Oleg23.OK@gmail.com>
2019-11-16 14:57:26 +0200

    work started

--- src/main/java/test/testvolodya/CreateAddressVerifyExpected.java
+++ src/main/java/test/testvolodya/CreateAddressVerifyExpected.java
@@ -16,14 +16,14 @@ import org.testng.annotations.*;
 import main.java.pages.addresses.*;
 import main.java.data.AddressRepository;
 import main.java.pages.user.UserPage;
-import main.java.test.test;
+import main.java.test.BasicTest;
 import static org.testng.Assert.assertEquals;
 
 // Listeners are waiting for the error and if it occurs - make a screenshot of error page
-@Listeners(test.class)
+@Listeners(BasicTest.class)
 
 // CreateAddress class
-public class CreateAddressVerifyExpected extends test {
+public class CreateAddressVerifyExpected extends BasicTest {
 
     @Severity(SeverityLevel.MINOR)
     @Description("Verify that new address entry match expected")
