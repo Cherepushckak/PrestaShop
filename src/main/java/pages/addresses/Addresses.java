defdc6c8992f3625cdf554da9ad0dbf06f16d5c3
VolodymyrRomanovych <volodymyr.romanovych@gmail.com>
2019-11-16 17:27:27 +0200

    Mega update of my own code =)

--- src/main/java/pages/addresses/Addresses.java
+++ src/main/java/pages/addresses/Addresses.java
@@ -12,6 +12,8 @@ package main.java.pages.addresses;
 
 // Additional packages
 import java.util.ArrayList;
+
+import io.qameta.allure.Step;
 import org.openqa.selenium.WebElement;
 import main.java.tools.OurWebDriver;
 
@@ -20,13 +22,11 @@ public class Addresses {
 
     // Fields
     private WebElement addressesList;
-    private ArrayList<Address> addressesContainer;
     private WebElement createNewAddressLink;
+    private ArrayList<Address> addressesContainer;
     private WebElement alert;
 
-    /**
-     * Default constructor
-     */
+    // Default constructor
     public Addresses() {}
 
     public Addresses(WebElement addressesList) {
@@ -36,7 +36,7 @@ public class Addresses {
         alert();
     }
 
-    // Initialise list of addresses
+    @Step("Get addresses collection")
     private void initAddressesContainer() {
         addressesContainer = new ArrayList<>();
         for (WebElement current : OurWebDriver.driver.findElementsByCssSelector("div.col-sm-6:nth-child(2)")) {
@@ -44,27 +44,36 @@ public class Addresses {
         }
     }
 
-    // Initialise '+ Create new address' link
+    @Step("Initialize '+ Create new address' link")
     private void createNewAddressLink() {
         createNewAddressLink = OurWebDriver.driver.findElementByXPath("//section[@id='content']/div[*]/a");
     }
 
-    // Initialise 'Alert'
+    @Step("Initialize 'Alert'")
     private void alert() {
         alert = OurWebDriver.driver.findElementByXPath("//aside[@id='notifications']/div/article/ul");
     }
 
     // Getters
-    public WebElement getAddressesList() { return addressesList; }
+    @Step("Return addresses list")
+    public WebElement getAddressesList() {
+        return addressesList;
+    }
+
+    @Step("Return alert")
     public WebElement getAlert() {
         alert();
         return alert; }
 
+    @Step("Initialize list of addresses")
     public ArrayList<Address> getAddressesContainer() {
         initAddressesContainer();
         return addressesContainer;
     }
 
-    // Click '+ Create new address' link
-    public void clickCreateNewAddressLink() { createNewAddressLink.click(); }
+    @Step("Click '+ Create new address' link")
+    public NewAddress clickCreateNewAddressLink() {
+        createNewAddressLink.click();
+        return new NewAddress();
+    }
 }
