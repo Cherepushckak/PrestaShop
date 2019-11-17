19cfe6048c68ae52b0f071990bc0a43e7c685f2f
VolodymyrRomanovych <volodymyr.romanovych@gmail.com>
2019-11-17 13:46:58 +0200

    Updated my files to actual version

--- src/main/java/test/volodya/EditAddress.java
+++ src/main/java/test/volodya/EditAddress.java
@@ -17,12 +17,10 @@ import main.java.pages.user.Address;
 import main.java.pages.header.CertainLanguage;
 import main.java.helperinstrument.LogInHelper;
 import main.java.data.AddressRepository;
-
 import main.java.pages.user.UserPage;
 import main.java.test.BasicTest;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
-
 import static org.testng.Assert.assertEquals;
 
 // Listeners are waiting for the error and if it occurs - make a screenshot of error page
@@ -36,12 +34,14 @@ public class EditAddress extends BasicTest {
     @Description("Verify that address entry can be updated")
     @Test
     public void editAddress() {
+        // Arrange
         // Log In to PrestaShop
         UserPage userPage = new LogInHelper().getUserPage();
 
         // Change language to English
         CertainLanguage certainLanguage = new CertainLanguage().openListOfLanguages().chooseEnglishInDropDown();
 
+        // Act
         // Click 'Addresses' card-link
         Addresses addresses = new UserPage().getUserPageContainer().clickAddresses();
 
@@ -60,7 +60,6 @@ public class EditAddress extends BasicTest {
                 "addressComplement",
                 "zipPostalCode",
                 "city",
-                "country",
                 "phone");
 
         // Fill in all required fields from addresses repository from create address2.
@@ -70,6 +69,7 @@ public class EditAddress extends BasicTest {
         // Click 'SAVE' button
         Addresses addresses2 = new NewAddress().clickSaveButton();
 
+        // Assert
         // Verification, that address was successfully updated
         String actualAlert = new Addresses().getAlert().getText();
         String expectedAlert = "Address successfully updated!";
