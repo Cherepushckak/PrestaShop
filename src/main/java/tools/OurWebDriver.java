d7009f3cf4be1413a2b67f52c008271a9d2c071d
VolodymyrRomanovych <volodymyr.romanovych@gmail.com>
2019-11-28 05:56:41 +0200

    Small improve

--- src/main/java/tools/OurWebDriver.java
+++ src/main/java/tools/OurWebDriver.java
@@ -2,6 +2,7 @@ package main.java.tools;
 
 import main.java.helperinstrument.EnviromentHelper;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.concurrent.TimeUnit;
@@ -9,19 +10,19 @@ import java.util.concurrent.TimeUnit;
 public class OurWebDriver {
     static public ChromeDriver driver;
     private static WebDriverWait wait;
-    
+
     public OurWebDriver() {
 
-        if ((System.getProperty("os.name")).contains("Mac OS")) {
-            System.setProperty("webdriver.chrome.driver", EnviromentHelper.getMacDriver () );
-       } else {
-                System.setProperty("webdriver.chrome.driver", EnviromentHelper.getWinDriver ());
+        if ((System.getProperty("os.name")).contains("Mac OS") || (System.getProperty("os.name")).contains("Linux")) {
+            System.setProperty("webdriver.chrome.driver", EnviromentHelper.getMacDriver());
+        } else {
+            System.setProperty("webdriver.chrome.driver", EnviromentHelper.getWinDriver());
         }
 
         driver = new ChromeDriver();
         driver.manage().window().maximize(); // Maximize test browser window
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-        driver.get(EnviromentHelper.getPrestaUrl ()); // GoTo startURL
+        driver.get(EnviromentHelper.getPrestaUrl()); // GoTo startURL
         wait = new WebDriverWait(driver, 5);
     }
 
@@ -29,5 +30,7 @@ public class OurWebDriver {
         return wait;
     }
 
-    public void closeDriver() { driver.quit(); } // Close test browser windows to release hardware resources
+    public void closeDriver() {
+        driver.quit();
+    } // Close test browser windows to release hardware resources
 }
